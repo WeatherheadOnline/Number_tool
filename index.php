391b331feb043f2dba60cc967ab5dd696c16a820
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-09-02 07:31:20 +1000

    Functionality exists to calculate day and ruling numbers by either addition method as selected via  radio buttons.

--- index.php
+++ index.php
@@ -10,15 +10,29 @@
 <form name="dob-entry-form" method="POST" action="">
     <fieldset>
         <legend>Enter your date of birth:</legend>
-        <label for="dob">Date of birth
+
+        <label>Date of birth
             <input type="date" id="dob" name="dob" />
         </label>
+        
+        <label>
+            <input type="radio" id="all-together" name="addition-method" value="all-together" checked <?php if (isset($_POST['addition-method'])) {if($_POST['addition-method']=='all-together'){echo 'checked';}} ?>>
+            Add all digits together at once (default)
+        </label>
+        <label>
+            <input type="radio" id="individually" name="addition-method" value="individually" <?php if (isset($_POST['addition-method'])) {if($_POST['addition-method']=='individually'){echo 'checked';}} ?>>
+            Add the day, month and year separately first 
+        </label>
+        
         <input type="reset" value="Reset" />
         <input type="submit" value="Submit" />
     </fieldset>
 </form>
 
 <?php
+
+
+
     function dataFilter($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -28,32 +42,28 @@
 
     $input = new DateTime(dataFilter($_POST['dob']));
     $dob = $input->format('Ynj');
-    $myYear = $input->format('Y');
-    $myMonth = $input->format('n'); # month number without a leading zero
     $myDay = $input->format('j'); # day of month without a leading zero
-
-    $allDigits = str_split((string)$dob);  # "digits" suffix denotes an array
-    $allSum = array_sum($allDigits);       # "sum" suffix denotes the sum of an array
-    $yearDigits = str_split((string)$myYear);
-    $yearSum = array_sum($yearDigits);
-    $monthDigits = str_split((string)$myMonth);
-    $monthSum = array_sum($monthDigits);
-    $dayDigits = str_split((string)$myDay);
-    $daySum = array_sum($dayDigits);
+    $myMonth = $input->format('n'); # month number withoun a leading zero
+    $myYear = $input->format('Y'); # year in four-digit format 
 
     function addThese($inputNumber) {
-        if($inputNumber % 11 == 0 || $inputNumber <=9) {
-            echo 'Returning input number as: ' . $inputNumber . '<br>';
+        if($inputNumber % 11 == 0 && $inputNumber <= 33 || $inputNumber <=9) {
             return $inputNumber;
         } else {
-            echo 'Continuing to iterate with: ' .$inputNumber . '<br>';
             return addThese(array_sum(str_split((string)$inputNumber)));
         }
     }
 
-    // $rulingNumber = addThese()
-    echo 'Running addThese directly on the set of digits, $dob: ' . addThese($dob) . '<br>';
-    echo 'Running addThese on just the day directly from the date: ' . addThese($myDay);
+    $radioSelection = $_POST['addition-method'];
+    if($radioSelection == 'all-together') {
+        echo 'Your ruling number is ' . addThese($dob) . ',<br>';
+    } else {
+        echo 'Via the alt method, your ruling number is ' . addThese(addThese($myDay) + addThese($myMonth) + addThese($myYear)) . ',<br>';
+    };
+        echo 'and your day number is ' . addThese($myDay);
+
+
+    
 ?>
 
     
