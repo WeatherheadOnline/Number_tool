0b9be3037583a009ee5966160c21663fe997a951
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-10-06 18:08:59 +1100

    Got cards rendering to the screen, from an array; laid out and styled as needed.

---
+++ src/components/Cards.js
@@ -0,0 +1,70 @@
+import React from "react";
+import '../css/Cards.css'
+
+const Cards = (props) => {
+    const cardsToDisplay = props.records.map(card => {
+
+        return (
+        <article key={card.key} className="card">
+            <TopRowOfCard />
+            <div className="card-name-date-wrapper">
+                {card.nExpression 
+                    ? <NameSideOfCard name={card.name} expr={card.nExpression} soul={card.nSoul} />
+                    : <NamelessSideOfCard nickname={card.name} />            
+                }
+                {card.date 
+                    ? <DateSideOfCard date={card.date} ruling={card.nRuling} day={card.nDay} />
+                    : <span className="add-a-date-btn add-btn">Add a date</span>
+                }
+            </div>
+        </article>
+       )
+    });
+
+    return (
+        <section className="cards-container">
+            {cardsToDisplay}
+        </section>
+    );
+};
+
+const TopRowOfCard = () => {
+    return (
+        <div className="top-row-of-card">
+            <span>Tags go here</span>
+            <span>Delete</span>
+        </div>
+    );
+};
+
+const NameSideOfCard = (props) => {
+    return (
+        <div className="card-name-or-date card-name">
+            <h2>{props.name}</h2>
+            <p>Expression number: <span className="outcome-number">{props.expr}</span></p>
+            <p>Soul number: <span className="outcome-number">{props.soul}</span></p>
+        </div>
+    );
+};
+
+const NamelessSideOfCard = (props) => {
+    return (
+        <div className="card-name-or-date card-name">
+            <p>Nickname:</p>
+            <h2 className="nickname">"{props.nickname}"</h2>
+            <p className="add-btn">Add a name</p>
+        </div>
+    )
+}
+
+const DateSideOfCard = (props) => {
+    return (
+        <div className="card-contents card-date">
+            <h2>{props.date}</h2>
+            <p>Ruling number: <span className="outcome-number">{props.ruling}</span></p>
+            <p>Day number: <span className="outcome-number">{props.day}</span></p>
+        </div>
+    );
+};
+
+export default Cards;
---
+++ src/components/People.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+const records = [
+    {
+        name: "Albert Einstein",
+        date: "14-03-1879",
+        year: 1879,
+        month: 3,
+        day: 14,
+        nRuling: 33,
+        nDay: 5,
+        nExpression: 9,
+        nSoul: 7,
+    },
+    {
+        name: "Maya Angelou",
+        date: "04-04-1928",
+        year: 1928,
+        month: 4,
+        day: 4,
+        nRuling: 1,
+        nDay: 4,
+        nExpression: 7,
+        nSoul: 8,
+    },
+    {
+        name: "Lin-Manuel Miranda",
+        date: "16-01-1980",
+        year: 1980,
+        month: 1,
+        day: 16,
+        nRuling: 8,
+        nDay: 7,
+        nExpression: 8,
+        nSoul: 11,
+    },
+    {
+        name: 'Alfred Matthew "Weird Al" Yankovic',
+        date: "23-10-1959",
+        year: 1959,
+        month: 10,
+        day: 23,
+        nRuling: 3,
+        nDay: 5,
+        nExpression: 11,
+        nSoul: 1,
+    },
+    {
+        name: "Rosalind Franklin",
+        date: "25-07-1920",
+        year: 1920,
+        month: 7,
+        day: 25,
+        nRuling: 8,
+        nDay: 7,
+        nExpression: 6,
+        nSoul: 8,
+    },
+]
+
+const displayRecords = records.map((person, index) => {
+    return (
+        <div class="person">
+            <h2>Name: {person.name}</h2>
+            <p>Date: {person.day}-{person.month}-{person.year}</p>
+            <p>Ruling number: {person.nRuling}</p>
+            <p>Day number: {person.nDay}</p>
+            <p>Expression number: {person.nExpression}</p>
+            <p>Soul number: {person.nSoul}</p>
+        </div>
+    );
+})
+
+const People = () => {
+    return (
+        <div>
+            {displayRecords}
+        </div>
+    );
+}
+
+export default People;
