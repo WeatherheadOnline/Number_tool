1045bf46fe5ef29a69ce94dec1d4696b314708ee
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-12-04 09:17:52 +1100

    Added book references to the 'about' section.

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ExternalLink from './ExternalLink';
 
 const About = () => {
 
@@ -17,6 +18,22 @@ const About = () => {
 				<h2>About this page</h2>
 				<p>Your information is safe: this site never puts any of your information online. Any info you enter gets stored on your device, and never leaves.</p>
 				<p>This site was made because most apps only allow one person's information to be added, and the author wanted a way to record lots of peoples' basic number charts in one place.</p>
+
+				<h2>About the numbers</h2>
+				<p>
+					This site uses definitions from Rosemaree Templeton's " 
+					<ExternalLink url="https://www.rockpoolpublishing.com/numerology">
+						Numerology: Numbers And Their Influence
+					</ExternalLink>
+					" (ISBN13: 9781925429022), and from Matthew Oliver Goodwin's " 
+					<ExternalLink url="https://www.goodreads.com/book/show/934801.Numerology_the_Complete_Guide_Volume_I">
+						Numerology The Complete Guide, Volume I
+					</ExternalLink>
+					" (ISBN-13: 978-1564148599).
+				</p>
+				
+
+
 				<h2>Contact</h2>
 				<p>Please get in touch if you have any questions, comments or concerns, or if you'd like to say hi.</p>
 				<p><a href="mailto:numerology@weatherheadonline.com">numerology@weatherheadonline.com</a></p>
